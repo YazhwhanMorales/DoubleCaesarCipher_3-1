16ee273ed86f0ab4108c8f9c22ffab958db1a19d
YazhwhanMorales <developeryazhwhan@gmail.com>
2025-11-23 19:27:43 -0600

    Elimination of redundant variable

--- caesarCipher_3-1.cpp
+++ caesarCipher_3-1.cpp
@@ -6,24 +6,21 @@ int main(){
 
     char cifrado[]= "Vpop md";
     int longitud = strlen(cifrado);
-    int contadorCaracter = 0;
 
     cout << "Frase cifrada con Cesar doble (+3/+1)"<< cifrado<<endl;
     
     for(int i=0; i<longitud; i++){
         if (cifrado[i] == ' '){
-            contadorCaracter++;
             continue;
         }
         else{
             //el 0 cuenta como par, por lo que a las pocisiones pares les restaremos 3 para modificar su ASCII 
             // a las posiciones impares les restamos 1 para asi cumplir correctamente con la alternacion del cifrado.
-            if (contadorCaracter % 2 == 0) {
+            if (i % 2 == 0) {
             cifrado[i] -= 3;
             } else {
             cifrado[i] -= 1;
             } 
-            contadorCaracter++;
         }
     }
     cout<< "Frase descifrada: "<< endl;
