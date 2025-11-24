e85935e4c6bc1683cc2be36e662d023d0ee201d8
YazhwhanMorales <developeryazhwhan@gmail.com>
2025-11-23 19:14:35 -0600

    corrected comment

--- caesarCipher_3-1.cpp
+++ caesarCipher_3-1.cpp
@@ -16,7 +16,8 @@ int main(){
             continue;
         }
         else{
-            //el 0 cuenta como par, por lo que a las pocisiones pares les restaremos 3 para modificar su ASCII y a las posiciones impares les restamos 1 para asi cumplir correctamente con la alternacion del cifrado.
+            //el 0 cuenta como par, por lo que a las pocisiones pares les restaremos 3 para modificar su ASCII 
+            // a las posiciones impares les restamos 1 para asi cumplir correctamente con la alternacion del cifrado.
             if (contadorCaracter % 2 == 0) {
             cifrado[i] -= 3;
             } else {
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+    int contadorCaracter = 0;
